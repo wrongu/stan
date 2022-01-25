58d94bddcd7fd22b2a73816a61077c0fa4556bf2
wrongu <richard.d.lange.13@dartmouth.edu>
2022-01-25 17:23:24 -0500

    bare-bone implementation of ISVI (Interpolating Sampling and Variational Inference)
    Issues:
    - results are in terms of unconstrained parameters of the wrapped model
    - decoupled from existing variational::normal_meanfield implementation
    - no tests yet
    - only verified on bernoulli example

---
+++ src/stan/services/experimental/isvi/isvi_stams_wrapper.hpp
@@ -0,0 +1,275 @@
+#ifndef STAN_ISVI_STAMS_MODEL_WRAPPER_HPP
+#define STAN_ISVI_STAMS_MODEL_WRAPPER_HPP
+
+#include <stan/math.hpp>
+#include <stan/model/model_base_crtp.hpp>
+#include <stan/callbacks/logger.hpp>
+#include <stdexcept>
+#include <ostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace stan {
+namespace isvi {
+
+/** ------------------------------------------------------------------
+ * TEMPORARY - GETTING AROUND TYPING ISSUES WITH NORMAL_MEANFIELD BY 
+ * REIMPLEMENTING THE KEY FUNCTIONS WE NEED FROM IT HERE WITH TEMPLATE<T>
+ */
+template <typename T>
+inline T q_entropy(Eigen::Matrix<T, -1, 1>& params_r, int dim){
+    auto log_sigma_ = params_r.tail(dim);
+    double d = static_cast<double>(dim);
+    return 0.5 * d * (1.0 + stan::math::LOG_TWO_PI) + log_sigma_.sum();
+  }
+
+template <typename T>
+inline T q_log_det_fisher(Eigen::Matrix<T, -1, 1>& params_r, int dim){
+  auto log_sigma_ = params_r.tail(dim);
+  return -2.0 * log_sigma_.sum();
+}
+
+template <typename T, class BaseRNG>
+inline Eigen::Matrix<T, -1, 1> q_sample(Eigen::Matrix<T, -1, 1>& params_r, int dim, BaseRNG& rng){
+  // Initialize to the mean
+  auto z_ = params_r.head(dim);
+  auto sigma_ = stan::math::exp(params_r.tail(dim));
+  // Perturb each dimension by random noise proportional to sigma
+  for (int d=0; d<dim; ++d)
+    z_[d] += sigma_[d] * stan::math::normal_rng(0, 1, rng);
+  return z_;
+}
+/** ------------------------------------------------------------------ */
+
+/** Model wrapper class derives from the base class for models, i.e. stan::model::model_base_crtp.
+ * It wraps a given model with a variational approximation, and provides "log prob" gradients
+ * for the variational parameters.
+ * 
+ * Note on CRTP pattern: the full typename of this class is isvi_stams_model_wrapper<Model,BaseRNG>,
+ * and in the CRTP pattern we make class Foo inherit from Bar<Foo>. Here, 'Bar' is model_base_crtp,
+ * and 'Foo' is isvi_stams_model_wrapper<Model,BaseRNG>. Hence some verbose nested templates.
+ */
+template <class Model, class BaseRNG>
+class isvi_stams_model_wrapper : public stan::model::model_base_crtp<isvi_stams_model_wrapper<Model, BaseRNG>> {
+ public:
+  isvi_stams_model_wrapper(Model& m, BaseRNG& rng, int n_monte_carlo_grad, int n_monte_carlo_kl, double lambda)
+  : stan::model::model_base_crtp<isvi_stams_model_wrapper<Model, BaseRNG>>(2*m.num_params_r()),
+  wrapped_(m),
+  rng_(rng),
+  n_monte_carlo_grad_(n_monte_carlo_grad),
+  n_monte_carlo_kl_(n_monte_carlo_kl),
+  lambda_(lambda) {
+    // Sanity checks on inputs
+    static const char* function = "stan::isvi::isvi_stams";
+    math::check_positive(function,
+      "Number of Monte Carlo samples for gradients",
+      n_monte_carlo_grad_);
+    math::check_positive(function,
+      "Number of Monte Carlo samples for KL",
+      n_monte_carlo_kl_);
+    math::check_positive(function,
+      "Lambda must be greater than or equal to 1.0",
+      lambda - 1.0);
+  }
+
+  // ===== BEGIN BLOCK COPIED FROM ADVI =====
+
+  /**
+   * Modified copy of calc_ELBO from ADVI
+   */
+  template<typename T>
+  T kl_q_p(Eigen::Matrix<T,-1,1>& theta) const {
+    static const char* function = "stan::isvi::kl_q_p";
+
+    T kl = 0.0;
+    int dim = wrapped_.num_params_r();
+    int n_dropped_evaluations = 0;
+    for (int i = 0; i < n_monte_carlo_kl_;) {
+      auto zeta = q_sample<T>(theta, dim, rng_);
+      try {
+        std::stringstream ss;
+        T log_prob = wrapped_.template log_prob<false, true, T>(zeta, &ss);
+        stan::math::check_finite(function, "log_prob", log_prob);
+        kl -= log_prob;
+        ++i;
+      } catch (const std::domain_error& e) {
+        ++n_dropped_evaluations;
+        if (n_dropped_evaluations >= n_monte_carlo_kl_) {
+          const char* name = "The number of dropped evaluations";
+          const char* msg1 = "has reached its maximum amount (";
+          const char* msg2
+              = "). Your model may be either severely "
+                "ill-conditioned or misspecified.";
+          stan::math::throw_domain_error(function, name, n_monte_carlo_kl_,
+                                         msg1, msg2);
+        }
+      }
+    }
+    kl /= n_monte_carlo_kl_;
+    kl -= q_entropy<T>(theta, dim);
+    return kl;
+  }
+  // ===== END BLOCK COPIED FROM ADVI =====
+
+
+  // ===== BEGIN OVERRIDING MODEL_BASE_CRTP DEFINITIONS =====
+  /**
+   * Return the name of the model.
+   *
+   * @return model name
+   */
+  std::string model_name() const override {
+    return "isvi_lambda" + std::to_string(lambda_) + "_" + wrapped_.model_name();
+  }
+
+  /**
+   * Returns the compile information of the model:
+   * stanc version and stanc flags used to compile the model.
+   *
+   * @return model name
+   */
+  std::vector<std::string> model_compile_info() const override {
+    return wrapped_.model_compile_info();
+  }
+
+  /**
+   * Set the specified argument to sequence of parameters, transformed
+   * parameters, and generated quantities in the order in which they
+   * were declared.  The input sequence is cleared and resized.
+   *
+   * @param[in,out] names sequence of names parameters, transformed
+   * parameters, and generated quantities
+   */
+  void get_param_names(std::vector<std::string>& names) const override {
+    std::vector<std::string> wrapped_names;
+    wrapped_.get_param_names(wrapped_names);
+    names.resize(0);
+    for (std::string dim_name : wrapped_names)
+      names.push_back("mu_" + dim_name);
+    for (std::string dim_name : wrapped_names)
+      names.push_back("omega_" + dim_name);
+  }
+
+  /**
+   * Set the dimensionalities of constrained parameters, transformed
+   * parameters, and generated quantities.  The input sequence is
+   * cleared and resized.  The dimensions of each parameter
+   * dimensionality is represented by a sequence of sizes.  Scalar
+   * real and integer parameters are represented as an empty sequence
+   * of dimensions.
+   *
+   * <p>Indexes are output in the order they are used in indexing. For
+   * example, a 2 x 3 x 4 array will have dimensionality
+   * `std::vector<size_t>{ 2, 3, 4 }`, whereas a 2-dimensional array
+   * of 3-vectors will have dimensionality `std::vector<size_t>{ 2, 3
+   * }`, and a 2-dimensional array of 3 x 4 matrices will have
+   * dimensionality `std::vector<size_t>{2, 3, 4}`.
+   *
+   * @param[in,out] dimss sequence of dimensions specifications to set
+   */
+  void get_dims(std::vector<std::vector<size_t> >& dimss) const override {
+    dimss.resize(0);
+    // We have only unconstrained parameters, so num constrained = num unconstrained
+    dimss.emplace_back(std::vector<size_t>{this->num_params_r()});
+    // We have zero transformed parameters
+    dimss.emplace_back(std::vector<size_t>{});
+    // We have zero generated quantities
+    dimss.emplace_back(std::vector<size_t>{});
+  }
+
+  void constrained_param_names(std::vector<std::string>& param_names,
+                               bool include_tparams,
+                               bool include_gqs) const override {
+    // Since the variational approximation is over the *unconstrained*
+    // params in the wrapped model, we still use those same names for 
+    // the *constrained* params here.
+    std::vector<std::string> wrapped_names;
+    wrapped_.unconstrained_param_names(wrapped_names);
+    // param_names.resize(0);
+    for (std::string dim_name : wrapped_names)
+      param_names.push_back("mu_" + dim_name);
+    for (std::string dim_name : wrapped_names)
+      param_names.push_back("omega_" + dim_name);
+  }
+
+  void unconstrained_param_names(std::vector<std::string>& param_names,
+                                 bool include_tparams,
+                                 bool include_gqs) const override {
+    std::vector<std::string> wrapped_names;
+    wrapped_.unconstrained_param_names(wrapped_names);
+    // param_names.resize(0);
+    for (std::string dim_name : wrapped_names)
+      param_names.push_back("mu_" + dim_name);
+    for (std::string dim_name : wrapped_names)
+      param_names.push_back("omega_" + dim_name);
+  }
+
+  template <bool propto, bool jacobian, typename T>
+  T log_prob(Eigen::Matrix<T, -1, 1>& params_r,
+             std::ostream* msgs) const {
+    T log_det_fisher = q_log_det_fisher<T>(params_r, wrapped_.num_params_r());
+    T kl_qp = kl_q_p(params_r);
+    return 0.5*log_det_fisher - lambda_ * kl_qp;
+  }
+
+  template <bool propto, bool jacobian, typename T>
+  T log_prob(std::vector<T>& params_r, std::vector<int>& params_i,
+             std::ostream* msgs) const {
+    // Cast vector -> matrix and just redirect to the above implementation
+    Eigen::Matrix<T, -1, 1> eigen_params_r(params_r.size());
+    for (int i=0; i<params_r.size(); ++i)
+      eigen_params_r[i] = params_r[i];
+    return this->template log_prob<propto, jacobian, T>(eigen_params_r, msgs);
+  }
+
+  /**
+   * Read constrained parameter values from the specified context,
+   * unconstrain them, then concatenate the unconstrained sequences
+   * into the specified parameter sequence.  Output messages go to the
+   * specified stream.
+   *
+   * @param[in] context definitions of variable values
+   * @param[in,out] params_r unconstrained parameter values produced
+   * @param[in,out] msgs stream to which messages are written
+   */
+  void transform_inits(const io::var_context& context, Eigen::VectorXd& params_r,
+                       std::ostream* msgs) const override {
+    throw new std::runtime_error("stan::isvi::isvi_stams_model_wrapper::transform_inits not implemented!");
+  }
+
+  void transform_inits(const stan::io::var_context& context, std::vector<int>& params_i,
+                       std::vector<double>& params_r, std::ostream* msgs) const override {
+    throw new std::runtime_error("stan::isvi::isvi_stams_model_wrapper::transform_inits not implemented!");
+  }
+
+  template <typename RNG>
+  void write_array(RNG& base_rng, Eigen::VectorXd& params_r,
+                   Eigen::VectorXd& params_constrained_r, bool include_tparams,
+                   bool include_gqs, std::ostream* msgs) const {
+    params_constrained_r = params_r;
+  }
+
+  template <typename RNG>
+  void write_array(RNG& base_rng, std::vector<double>& params_r,
+                   std::vector<int>& params_i,
+                   std::vector<double>& params_r_constrained,
+                   bool include_tparams, bool include_gqs,
+                   std::ostream* msgs) const {
+    params_r_constrained = params_r;
+  }
+
+  // ===== END OVERRIDING MODEL_BASE_CRTP =====
+
+ protected:
+  Model& wrapped_;
+  BaseRNG& rng_;
+  int n_monte_carlo_grad_;
+  int n_monte_carlo_kl_;
+  double lambda_;
+};
+
+}  // namespace isvi
+}  // namespace stan
+
+#endif
---
+++ src/stan/services/experimental/isvi/nuts_diag_e_meanfield_q.hpp
@@ -0,0 +1,122 @@
+#ifndef STAN_SERVICES_EXPERIMENTAL_ISVI_MEANFIELD_HPP
+#define STAN_SERVICES_EXPERIMENTAL_ISVI_MEANFIELD_HPP
+
+#include <stan/callbacks/interrupt.hpp>
+#include <stan/callbacks/logger.hpp>
+#include <stan/callbacks/writer.hpp>
+#include <stan/services/util/experimental_message.hpp>
+#include <stan/services/util/initialize.hpp>
+#include <stan/services/util/create_rng.hpp>
+#include <stan/io/var_context.hpp>
+#include <stan/services/experimental/isvi/isvi_stams_wrapper.hpp>
+#include <boost/random/additive_combine.hpp>
+#include <stan/mcmc/hmc/nuts/diag_e_nuts.hpp>
+#include <stan/services/util/run_sampler.hpp>
+#include <stan/services/util/create_rng.hpp>
+#include <stan/services/util/initialize.hpp>
+#include <stan/services/util/inv_metric.hpp>
+#include <string>
+#include <vector>
+
+namespace stan {
+namespace services {
+namespace experimental {
+namespace isvi {
+
+/**
+ * Runs ISVI algorithm with mean-field (diagonal Gaussian) Q
+ * 
+ * Based on adapting advi/meanfield service and sample/hmc_nuts_diag_e
+ * services
+ *
+ * @tparam Model A model implementation
+ * @param[in] model Input model to test (with data already instantiated)
+ * @param[in] init var context for initialization
+ * @param[in] init_inv_metric var context exposing an initial diagonal
+ *            inverse Euclidean metric (must be positive definite)
+ * @param[in] random_seed random seed for the random number generator
+ * @param[in] chain chain id to advance the random number generator
+ * @param[in] init_radius radius to initialize
+ * ---------- NUTS-like arguments ----------
+ * @param[in] num_warmup Number of warmup samples
+ * @param[in] num_samples Number of samples
+ * @param[in] num_thin Number to thin the samples
+ * @param[in] save_warmup Indicates whether to save the warmup iterations
+ * @param[in] refresh Controls the output
+ * @param[in] stepsize initial stepsize for discrete evolution
+ * @param[in] stepsize_jitter uniform random jitter of stepsize
+ * @param[in] max_depth Maximum tree depth
+ * ---------- ADVI-like arguments ----------
+ * @param[in] grad_samples number of samples for Monte Carlo estimate of gradients
+ * @param[in] kl_samples number of samples for Monte Carlo estimate of kl
+ * @param[in] lambda controls Sampling/VI trade-off
+ * ---------- Further common arguments ----------
+ * @param[in,out] interrupt callback to be called every sample
+ * @param[in,out] logger Logger for messages
+ * @param[in,out] init_writer Writer callback for unconstrained inits
+ * @param[in,out] sample_writer output for samples of parameter values
+ * @param[in,out] diagnostic_writer output for diagnostic values
+ * @return error_codes::OK if successful
+ */
+template <class Model>
+int nuts_diag_e_meanfield_q(Model& model, const stan::io::var_context& init,
+                            unsigned int random_seed, unsigned int chain,
+                            double init_radius, int num_warmup, int num_samples,
+                            int num_thin, bool save_warmup, int refresh,
+                            double stepsize, double stepsize_jitter, int max_depth,
+                            int grad_samples, int kl_samples, double lambda,
+                            callbacks::interrupt& interrupt,
+                            callbacks::logger& logger, 
+                            callbacks::writer& init_writer,
+                            callbacks::writer& sample_writer,
+                            callbacks::writer& diagnostic_writer) {
+  util::experimental_message(logger);
+
+  boost::ecuyer1988 rng = util::create_rng(random_seed, chain);
+
+  // Whereas 'model' is the compiled STAN model, 'wrapped_model' is the model we
+  // will pass to the sampler.
+  logger.debug("CREATING WRAPPED_MODEL");
+  stan::isvi::isvi_stams_model_wrapper<Model, boost::ecuyer1988>
+    wrapped_model(model, rng, grad_samples, kl_samples, lambda);
+
+  logger.debug("INITIALIZING");
+  std::vector<double> cont_vector = util::initialize(
+      wrapped_model, init, rng, init_radius, true, logger, init_writer);
+
+  logger.debug("CREATING INV_METRIC");
+  stan::io::dump dmp
+      = util::create_unit_e_diag_inv_metric(wrapped_model.num_params_r());
+  stan::io::var_context& unit_e_metric = dmp;
+
+  Eigen::VectorXd inv_metric;
+  try {
+    inv_metric = util::read_diag_inv_metric(unit_e_metric,
+                                            wrapped_model.num_params_r(),
+                                            logger);
+    util::validate_diag_inv_metric(inv_metric, logger);
+  } catch (const std::domain_error& e) {
+    return error_codes::CONFIG;
+  }
+
+  logger.debug("CREATING SAMPLER");
+  stan::mcmc::diag_e_nuts<Model, boost::ecuyer1988> sampler(wrapped_model, rng);
+
+  sampler.set_metric(inv_metric);
+  sampler.set_nominal_stepsize(stepsize);
+  sampler.set_stepsize_jitter(stepsize_jitter);
+  sampler.set_max_depth(max_depth);
+
+  logger.debug("RUNNING SAMPLER");
+  util::run_sampler(sampler, wrapped_model, cont_vector, num_warmup, num_samples,
+                    num_thin, refresh, save_warmup, rng, interrupt, logger,
+                    sample_writer, diagnostic_writer);
+
+  return error_codes::OK;
+}
+
+}  // namespace isvi
+}  // namespace experimental
+}  // namespace services
+}  // namespace stan
+#endif
